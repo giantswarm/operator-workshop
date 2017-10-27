b26880bc50293f03c1f261b0a2eb9c73d239671c
Paweł Kopiczko <kopiczko@gmail.com>
2017-10-27 09:14:48 +0200

    mysqlops: extend operations

--- mysqlops/mysqlops.go
+++ mysqlops/mysqlops.go
@@ -1,5 +1,10 @@
 package mysqlops
 
+type Database struct {
+	Name  string
+	Owner string
+}
+
 type Config struct {
 }
 
@@ -12,6 +17,18 @@ func New(config Config) (*MySQLOps, error) {
 	return mysqlOps, nil
 }
 
-func (m *MySQLOps) CreateDatabase(name string) error {
+func (m *MySQLOps) CreateDatabase(name, owner string) error {
 	return nil
+}
+
+func (m *MySQLOps) ChangeDatabaseOwner(name, owner string) error {
+	return nil
+}
+
+func (m *MySQLOps) DeleteDatabase(name string) error {
+	return nil
+}
+
+func (m *MySQLOps) ListDatabases() ([]Database, error) {
+	return []Database{}, nil
 }
