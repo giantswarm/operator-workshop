375043674cf4dd839d310c2b496651b509478fd1
Paweł Kopiczko <kopiczko@gmail.com>
2017-11-02 09:41:33 +0100

    solution2: add informer and reconciliation

--- solution2/solution2.go
+++ solution2/solution2.go
@@ -5,8 +5,8 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -16,6 +16,9 @@ import (
 	"time"
 
 	"github.com/giantswarm/operator-workshop/mysqlops"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/rest"
 
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
@@ -39,6 +42,9 @@ type Config struct {
 // MySQLConfig is custom object of mysqlconfigs.containerconf.de custom
 // resource.
 type MySQLConfig struct {
+	apismetav1.TypeMeta   `json:",inline"`
+	apismetav1.ObjectMeta `json:"metadata,omitempty"`
+
 	Spec MySQLConfigSpec `json:"spec"`
 }
 
@@ -83,7 +89,42 @@ func (s MySQLConfigSpec) Validate() error {
 // MySQLConfigList represents a list of custom objects. It is useful for
 // decoding list API calls.
 type MySQLConfigList struct {
+	apismetav1.TypeMeta `json:",inline"`
+	apismetav1.ListMeta `json:"metadata,omitempty"`
+
 	Items []*MySQLConfig `json:"items"`
+}
+
+// decoder decodes custom objects from a stream. It is used for decoding list
+// of objects by client-go watcher.
+type decoder struct {
+	stream  io.ReadCloser
+	jsonDec *json.Decoder
+}
+
+func newDecoder(stream io.ReadCloser) *decoder {
+	return &decoder{
+		stream:  stream,
+		jsonDec: json.NewDecoder(stream),
+	}
+}
+
+func (d *decoder) Decode() (action watch.EventType, object runtime.Object, err error) {
+	var e struct {
+		Type   watch.EventType
+		Object *MySQLConfig
+	}
+	e.Object = new(MySQLConfig)
+
+	if err := d.jsonDec.Decode(&e); err != nil {
+		return watch.Error, nil, err
+	}
+
+	return e.Type, e.Object, nil
+}
+
+func (d *decoder) Close() {
+	d.stream.Close()
 }
 
 func main() {
@@ -173,25 +214,29 @@ func mainError(ctx context.Context, config Config) error {
 		log.Printf("creating custom resource")
 
 		// crdJson content in YAML format can be found in crd.yaml file.
-		crd := apiextensionsv1beta1.CustomResourceDefinition{
-			APIVersion: "apiextensions.k8s.io/v1beta1",
-			Kind:       "CustomResourceDefinition",
-			Name:       "mysqlconfigs.containerconf.de",
+		crd := &apiextensionsv1beta1.CustomResourceDefinition{
+			TypeMeta: apismetav1.TypeMeta{
+				APIVersion: "apiextensions.k8s.io/v1beta1",
+				Kind:       "CustomResourceDefinition",
+			},
+			ObjectMeta: apismetav1.ObjectMeta{
+				Name: "mysqlconfigs.containerconf.de",
+			},
 			Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
 				Group:   "containerconf.de",
 				Version: "v1",
 				Scope:   apiextensionsv1beta1.NamespaceScoped,
-			},
-			Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
-				Plural:     "mysqlconfigs",
-				Singular:   "mysqlconfig",
-				Kind:       "MySQLConfig",
-				ShortNames: []string{},
+				Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
+					Plural:     "mysqlconfigs",
+					Singular:   "mysqlconfig",
+					Kind:       "MySQLConfig",
+					ShortNames: []string{},
+				},
 			},
 		}
 
-		res, err := k8sClient.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd)
-		if apierrors.IsAlreadyExists(crd) {
+		_, err := k8sClient.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd)
+		if apierrors.IsAlreadyExists(err) {
 			log.Printf("creating custom resource: already exists")
 		} else if err != nil {
 			return fmt.Errorf("creating custom resource: %s", err)
@@ -209,7 +254,7 @@ func mainError(ctx context.Context, config Config) error {
 		for ; ; attempt++ {
 			log.Printf("checking custom resource readiness attempt=%d", attempt)
 
-			res, err := k8sClient.ApiextensionsV1beta1().CustomResourceDefinitions().Get("mysqlconfigs.containerconf.de", apismetav1.GetOptions{})
+			_, err := k8sClient.ApiextensionsV1beta1().CustomResourceDefinitions().Get("mysqlconfigs.containerconf.de", apismetav1.GetOptions{})
 			if err != nil && attempt == maxAttempts {
 				return fmt.Errorf("checking custom resource readiness attempt=%d: %s", attempt, err)
 			} else if err != nil {
@@ -217,112 +262,154 @@ func mainError(ctx context.Context, config Config) error {
 				time.Sleep(checkInterval)
 			} else {
 				log.Printf("checking custom resource readiness attempt=%d: ready", attempt)
+				break
 			}
 		}
 	}
 
 	// Start reconciliation loop.
+
+	var watcher watch.Interface
+	{
+		endpoint := "/apis/containerconf.de/v1/watch/mysqlconfigs"
+
+		restClient := k8sClient.Discovery().RESTClient()
+
+		stream, err := restClient.Get().AbsPath(endpoint).Stream()
+		if err != nil {
+			return fmt.Errorf("creating a stream for endpoint=%s: %s", endpoint, err)
+		}
+
+		watcher = watch.NewStreamWatcher(newDecoder(stream))
+		defer watcher.Stop()
+	}
+
 	reconciliationCnt := 1
-	reconciliationInterval := time.Second * 1
 	for ; ; reconciliationCnt++ {
 		log.Printf("reconciling loopCnt=%d", reconciliationCnt)
 
-		if ctx.Err() == context.Canceled {
+		select {
+		case <-ctx.Done():
 			log.Printf("reconciling loopCnt=%d: context cancelled", reconciliationCnt)
 			return nil
-		}
-
-		url := config.K8sServer + "/apis/containerconf.de/v1/mysqlconfigs"
-		res, err := k8sClient.Get(url)
-		if err != nil {
-			return fmt.Errorf("reconciling loopCnt=%d: %s", reconciliationCnt, url, err)
-		}
-
-		body := readerToBytesTrimSpace(res.Body)
-		res.Body.Close()
-
-		if res.StatusCode != http.StatusOK {
-			log.Printf("reconciling loopCnt=%d: error client response status status=%d body=%#q", reconciliationCnt, res.StatusCode, body)
-			continue
-		}
-
-		var configs MySQLConfigList
-		err = json.Unmarshal(body, &configs)
-		if err != nil {
-			log.Printf("reconciling loopCnt=%d: error unmarshalling mysqlconfigs list body=%#q: %s", reconciliationCnt, body, err)
-			continue
-		}
-
-		for _, config := range configs.Items {
-			err := config.Validate()
+		case event := <-watcher.ResultChan():
+			obj, ok := event.Object.(*MySQLConfig)
+			if !ok {
+				return fmt.Errorf("reconciling loopCnt=%d: wrong type %T, want %T", reconciliationCnt, event.Object, &MySQLConfig{})
+			}
+			err := obj.Validate()
 			if err != nil {
-				log.Printf("reconciling loopCnt=%d: error invalid mysqlconfig=%#v: %s", reconciliationCnt, *config, err)
+				log.Printf("reconciling loopCnt=%d: error invalid obj=%#v: %s", reconciliationCnt, *obj, err)
 				continue
 			}
 
-			var ops *mysqlops.MySQLOps
-			{
-				ops, err = mysqlops.New(mysqlops.Config{})
+			switch event.Type {
+			case watch.Added, watch.Modified:
+				status, err := processUpdate(obj)
 				if err != nil {
-					log.Printf("reconciling loopCnt=%d obj=%#v: error creating MySQLOps: %s", reconciliationCnt, *config, err)
-					continue
-				}
-			}
-
-			// Reconcile MySQLConfig.
-			{
-				dbs, err := ops.ListDatabases()
-				if err != nil {
-					log.Printf("reconciling loopCnt=%d obj=%#v: error listing databases: %s", reconciliationCnt, *config, err)
-					continue
-				}
-
-				var foundDB mysqlops.Database
-				for _, db := range dbs {
-					if db.Name == config.Spec.Database {
-						foundDB = db
-						break
-					}
-				}
-
-				if foundDB.Name != "" {
-					db := foundDB
-
-					if db.Owner == config.Spec.Owner {
-						log.Printf("reconciling loopCnt=%d obj=%#v: object already reconcilled", reconciliationCnt, *config)
-						continue
-					}
-
-					log.Printf("reconciling loopCnt=%d obj=%#v: changing owner=%#q", reconciliationCnt, *config, db.Owner)
-					err := ops.ChangeDatabaseOwner(config.Spec.Database, config.Spec.Owner)
-					if err != nil {
-						log.Printf("reconciling loopCnt=%d obj=%#v: changing owner=%#q: error: %s", reconciliationCnt, *config, db.Owner, err)
-						continue
-					}
-					log.Printf("reconciling loopCnt=%d obj=%#v: changing owner=%#q: changed", reconciliationCnt, *config, db.Owner)
+					log.Printf("reconciling loopCnt=%d: error: processing update obj=%#v: %s", reconciliationCnt, *obj, err)
 				} else {
-					log.Printf("reconciling loopCnt=%d obj=%#v: creating database", reconciliationCnt, *config, err)
-					err := ops.CreateDatabase(config.Spec.Database, config.Spec.Owner)
-					if err != nil {
-						log.Printf("reconciling loopCnt=%d obj=%#v: creating database: error: %s", reconciliationCnt, *config, err)
-						continue
-					}
-					log.Printf("reconciling loopCnt=%d obj=%#v: creating database: created", reconciliationCnt, *config)
+					log.Printf("reconciling loopCnt=%d: reconciled: %s obj=%#v", reconciliationCnt, status, *obj)
 				}
+			case watch.Deleted:
+				status, err := processDelete(obj)
+				if err != nil {
+					log.Printf("reconciling loopCnt=%d: error: processing delete obj=%#v: %s", reconciliationCnt, *obj, err)
+				} else {
+					log.Printf("reconciling loopCnt=%d: reconciled: %s obj=%#v", reconciliationCnt, status, *obj)
+				}
+			default:
+				log.Printf("reconciling loopCnt=%d: error: unknown event type=%#v, unhandled event=%#v", reconciliationCnt, event.Type, event)
 			}
-
 		}
-
-		log.Printf("reconciling loopCnt=%d: reconciled", reconciliationCnt)
-		time.Sleep(reconciliationInterval)
 	}
 
 	return nil
 }
 
+func processUpdate(obj *MySQLConfig) (status string, err error) {
+	var ops *mysqlops.MySQLOps
+	{
+		ops, err = mysqlops.New(mysqlops.Config{})
+		if err != nil {
+			return "", fmt.Errorf("creating MySQLOps: %s", err)
+		}
+	}
+
+	// Reconcile MySQLConfig.
+	{
+		dbs, err := ops.ListDatabases()
+		if err != nil {
+			return "", fmt.Errorf("listing databases: %s", err)
+		}
+
+		var foundDB mysqlops.Database
+		for _, db := range dbs {
+			if db.Name == obj.Spec.Database {
+				foundDB = db
+				break
+			}
+		}
+
+		if foundDB.Name == "" {
+			err := ops.CreateDatabase(obj.Spec.Database, obj.Spec.Owner)
+			if err != nil {
+				return "", fmt.Errorf("creating database: %s", err)
+			}
+			return "database created", nil
+		}
+
+		if foundDB.Owner != obj.Spec.Owner {
+			err := ops.ChangeDatabaseOwner(obj.Spec.Database, obj.Spec.Owner)
+			if err != nil {
+				return "", fmt.Errorf("chaning owner=%#q: %s", foundDB.Owner, err)
+			}
+			return fmt.Sprintf("owner=%#q changed", foundDB.Owner), nil
+		}
+
+		return "already reconcilied", nil
+	}
+}
+
+func processDelete(obj *MySQLConfig) (status string, err error) {
+	var ops *mysqlops.MySQLOps
+	{
+		ops, err = mysqlops.New(mysqlops.Config{})
+		if err != nil {
+			return "", fmt.Errorf("creating MySQLOps: %s", err)
+		}
+	}
+
+	// Reconcile MySQLConfig.
+	{
+		dbs, err := ops.ListDatabases()
+		if err != nil {
+			return "", fmt.Errorf("listing databases: %s", err)
+		}
+
+		var foundDB mysqlops.Database
+		for _, db := range dbs {
+			if db.Name == obj.Spec.Database {
+				foundDB = db
+				break
+			}
+		}
+
+		if foundDB.Name != "" {
+			err := ops.DeleteDatabase(obj.Spec.Database)
+			if err != nil {
+				return "", fmt.Errorf("deleting database: %s", err)
+			}
+			return "database deleted", nil
+		}
+
+		return "already reconcilied", nil
+	}
+}
+
 // newK8sExtClient creates Kubernets extensions API client.
 func newK8sExtClient(config Config) (apiextensionsclient.Interface, error) {
-	restConfig = &rest.Config{
+	restConfig := &rest.Config{
 		Host: config.K8sServer,
 		TLSClientConfig: rest.TLSClientConfig{
 			CertFile: config.K8sCrtFile,
@@ -330,4 +417,6 @@ func newK8sExtClient(config Config) (apiextensionsclient.Interface, error) {
 			CAFile:   config.K8sCAFile,
 		},
 	}
+
+	return apiextensionsclient.NewForConfig(restConfig)
 }
