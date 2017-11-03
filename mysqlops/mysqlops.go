974ef501c025535965aa8151d07cf052a6515cad
Paweł Kopiczko <kopiczko@gmail.com>
2017-11-03 09:16:41 +0100

    mysqlops: fake implementation

--- mysqlops/mysqlops.go
+++ mysqlops/mysqlops.go
@@ -1,5 +1,17 @@
 package mysqlops
 
+import (
+	"errors"
+	"sync"
+)
+
+// This is a fake implementaiton so it has global variables, which represent
+// database server.
+var (
+	databases = make(map[string]Database)
+	mux       = new(sync.Mutex)
+)
+
 type Database struct {
 	Name  string
 	Owner string
@@ -8,6 +20,7 @@ type Database struct {
 type Config struct {
 }
 
+// MySQLOps is a fake MySQL operatations. To be implemented.
 type MySQLOps struct {
 }
 
@@ -18,17 +31,56 @@ func New(config Config) (*MySQLOps, error) {
 }
 
 func (m *MySQLOps) CreateDatabase(name, owner string) error {
+	mux.Lock()
+	defer mux.Unlock()
+
+	databases[name] = Database{
+		Name:  name,
+		Owner: owner,
+	}
+
 	return nil
 }
 
 func (m *MySQLOps) ChangeDatabaseOwner(name, owner string) error {
+	mux.Lock()
+	defer mux.Unlock()
+
+	db, ok := databases[name]
+
+	if !ok {
+		return errors.New("not found")
+	}
+
+	db.Owner = owner
+	databases[db.Name] = db
+
 	return nil
 }
 
 func (m *MySQLOps) DeleteDatabase(name string) error {
+	mux.Lock()
+	defer mux.Unlock()
+
+	_, ok := databases[name]
+
+	if !ok {
+		return errors.New("not found")
+	}
+
+	delete(databases, name)
+
 	return nil
 }
 
 func (m *MySQLOps) ListDatabases() ([]Database, error) {
-	return []Database{}, nil
+	mux.Lock()
+	defer mux.Unlock()
+
+	var dbs []Database
+	for _, db := range databases {
+		dbs = append(dbs, db)
+	}
+
+	return dbs, nil
 }
